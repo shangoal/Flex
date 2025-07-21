a6f9566ba5579786c56c4a90bd35fd177e61bb5f
Flex-MD <sandarunethshanhome@gmail.com>
2025-07-21 15:48:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "eVEWnbQY#b_5EOUNodI6Zeq1mnuu_tX3UM8Q046Myo3weopYMfIE",
+  SESSION_ID: process.env.SESSION_ID || "jRF2RKgD#0Xb3k-hSBxBQFw0gOQWSBqQLXzcwFlrQe-uMt-HA7XU",
   MONGODB: process.env.MONGODB || "mongodb://mongo:ylLlhjbpxHjKTqYkIqAaSinushsobfaK@yamabiko.proxy.rlwy.net:59692",
   OWNER_NUM: process.env.OWNER_NUM || "94764527598",
 };
