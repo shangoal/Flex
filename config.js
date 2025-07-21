c261ac1c1c5710ed23bb32b6ddf391efe6840db8
Flex-MD <sandarunethshanhome@gmail.com>
2025-07-21 18:25:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "jRF2RKgD#0Xb3k-hSBxBQFw0gOQWSBqQLXzcwFlrQe-uMt-HA7XU",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:ylLlhjbpxHjKTqYkIqAaSinushsobfaK@yamabiko.proxy.rlwy.net:59692",
+  
   OWNER_NUM: process.env.OWNER_NUM || "94764527598",
 };
